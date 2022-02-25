4798d4e21e08a4c668c95c6c48f9965a4d25b4e4
Sanchous4 <74542871+Sanchous4@users.noreply.github.com>
2022-02-25 06:08:41 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ExpressNode = require('express')();
 const PORT = process.env.PORT || 3000;
 
 ExpressNode.get('', (request, response) => {
-    response.send('Hello, world. bc7ee059 is the polls index.');
+    response.send('Hello, world. 65b02eb3 is the polls index.');
 });
 
 ExpressNode.listen(PORT, () => {
